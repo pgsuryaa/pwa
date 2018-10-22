10f7696765631ba6dde0aa831b6f7e95b7a9e0e6
pgsuryaa500987 <pgsuryaa111@gmail.com>
2018-10-22 15:38:32 +0400

    push

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -4,6 +4,22 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("pwa.gitzpwa_git.controller.Home", {
-
+		onInit: function() {
+			setTimeout(this._showNotification(), 10000);
+		},
+		_showNotification: function() {
+			navigator.serviceWorker.getRegistration().then(function(reg) {
+				var options = {
+					body: 'Here is a notification body!',
+					icon: 'icons/icon-152x152.png',
+					vibrate: [100, 50, 100],
+					data: {
+						dateOfArrival: Date.now(),
+						primaryKey: 1
+					}
+				};
+				reg.showNotification('Hello world!', options);
+			});
+		}
 	});
 });
